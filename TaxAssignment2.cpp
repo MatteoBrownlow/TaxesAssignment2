e3dcea40ccfde3597f71a7b458e66f6cc1720498
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-09-09 11:09:45 -0700

    Add files via upload

---
+++ TaxAssignment2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double income = 95000;
+    int estateTax = (4/100) * income;
+    int countyTax = (2/100) * income;
+    cout << "Estate tax is 4 percent, so " << estateTax << endl;
+    cout << "County tax is 2 percent, so " << countyTax << endl;
+    cout << "Total taxes are: " << estateTax + countyTax << endl;
+    cout << "Remaining income: " << income - (estateTax + countyTax) << endl;
+    return 0;
+}
