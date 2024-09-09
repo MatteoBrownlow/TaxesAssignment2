f8832d183e11908902e21eced36f62068fc4e54e
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-09-09 11:17:07 -0700

    Update TaxAssignment2.cpp
    fixed tax variables

--- TaxAssignment2.cpp
+++ TaxAssignment2.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(){
     double income = 95000;
-    int estateTax = (4/100) * income;
-    int countyTax = (2/100) * income;
+    int estateTax = (4.0/100) * income;
+    int countyTax = (2.0/100) * income;
     cout << "Estate tax is 4 percent, so " << estateTax << endl;
     cout << "County tax is 2 percent, so " << countyTax << endl;
     cout << "Total taxes are: " << estateTax + countyTax << endl;
